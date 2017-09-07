c2cc9953f7c88e3f8cf6e3c6c10f3701d698ee6d
Atticus Zambrana <angusbeefsteak14@gmail.com>
2017-09-07 12:02:28 -0700

    Changed the file a tiny bit

---
+++ src/main/java/net/angusbeefgaming/beefyanticheat/event/gamemodeCheck.java
@@ -0,0 +1,12 @@
+package net.angusbeefgaming.beefyanticheat.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import net.angusbeefgaming.beefyanticheat.Main;
+
+public class onPlayerMove implements Listener {
+	@EventHandler
+	public void whenPlayerMove(PlayerMoveEvent event) {
+  
+	}
+}
