8d65bbd6713054ba398ef3ca926c6596d83312b9
Atticus Zambrana <angusbeefsteak14@gmail.com>
2017-09-07 07:03:16 -0700

    Added kick message, and Staff alerts

--- src/main/java/net/angusbeefgaming/beefyanticheat/event/onPlayerMove.java
+++ src/main/java/net/angusbeefgaming/beefyanticheat/event/onPlayerMove.java
@@ -12,7 +12,12 @@ public class onPlayerMove implements Listener {
 			if (!event.getPlayer().hasPermission("essentials.fly")) {
 				if(!event.getPlayer().hasPermission("bac.bypass.fly")) {
 					event.getPlayer().setFlying(false);
-					event.getPlayer().kickPlayer("");
+					event.getPlayer().kickPlayer("[BAC CHEAT DETECTION]: Kicked for Fly Hacking");
+					for(Player p : Bukkit.getOnlinePlayers()) {
+						if(p.hasPermission("bac.alert")) {
+							p.sendMessage("[BAC CHEAT DETECTION]: Player " + event.getPlayer().getName() + " has been kicked for Fly Hacking!");
+						}
+					}
 				}
 				else {
 					
