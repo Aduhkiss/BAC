26ea2cfb0180e55dd906d9d46d3accf70b90ccec
Atticus Zambrana <angusbeefsteak14@gmail.com>
2017-09-08 15:23:35 -0700

    Updated file

--- src/main/java/net/angusbeefgaming/beefyanticheat/Main.java
+++ src/main/java/net/angusbeefgaming/beefyanticheat/Main.java
@@ -4,13 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public static Main plugin;
+	public String prefix = "[BAC CHEAT DETECTION]: ";
+	
+	
 	@Override
 	public void onEnable() {
 		getLogger().info("Beefy Anti-Cheat is Ready!");
-		// Register the Fly Check event
-		getServer().getPluginManager().registerEvents(this, new net.angusbeefgaming.beefyanticheat.event.onPlayerMove(), this);
+		// Register the BAC Command
+		getCommand("bac").setExecutor(new net.angusbeefgaming.beefyanticheat.command.BACCommand());
+		// Register /baccast Command
+		getCommand("baccast").setExecutor(new net.angusbeefgaming.beefyanticheat.command.BACBroadcast());
+		// Register the Speed check event
+		getServer().getPluginManager().registerEvents(new net.angusbeefgaming.beefyanticheat.event.onPlayerMove(), this);
 		// Register the Gamemode Check Event
-		getServer().getPluginManager().registerEvents(this, new net.angusbeefgaming.beefyanticheat.event.gamemodeCheck(), this);
+		getServer().getPluginManager().registerEvents(new net.angusbeefgaming.beefyanticheat.event.gamemodeCheck(), this);
 	}
 	@Override
 	public void onDisable() {
